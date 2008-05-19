5c2453f342ee06b70ba75ca83dd490dd069fd01c
Jason Roelofs <jameskilton@gmail.com>
2008-05-18 22:12:34 -0400

    started on the libnoise wrapper

---
+++ wrappers/noise/build_noise.rb
@@ -0,0 +1,22 @@
+# Wrap the libnoise library into Ruby
+
+require 'rbplusplus'
+include RbPlusPlus
+
+Extension.new "noise" do |e|
+  e.working_dir = File.expand_path(File.join(File.dirname(__FILE__),
+                                    "..", "..", "generated", "noise"))
+  e.sources File.expand_path(File.dirname(__FILE__) + "/../../tmp/noise/include/noise.h")
+
+  e.module "Noise" do |m|
+    m.namespace "noise"
+
+#    m.module "Model" do |model|
+#      model.namespace "model"
+#    end
+#
+#    m.module "Module" do |mod|
+#      mod.namespace "module"
+#    end
+  end
+end
