68da04910e4d4f8eba456b1d9c541a7d695e2a7b
Jason Roelofs <jameskilton@gmail.com>
2009-04-01 15:56:55 -0400

    Rice handles std::string already

--- wrappers/ogre/code/custom_to_from_ruby.cpp
+++ wrappers/ogre/code/custom_to_from_ruby.cpp
@@ -4,15 +4,3 @@ template<>
 Rice::Object to_ruby<short unsigned int>(const short unsigned int& x) {
   return Rice::Object(INT2NUM(x));
 }
-
-/*
-template<>
-std::string* from_ruby<std::string*>(Rice::Object x) {
-  return new std::string(from_ruby<char const*>(x));
-}
-
-template<>
-const std::string* from_ruby<const std::string*>(Rice::Object x) {
-  return new std::string(from_ruby<char const*>(x));
-}
-*/
