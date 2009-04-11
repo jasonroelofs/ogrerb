d5c640408657c063d21e7871308ba1c8096f20a2
Jason Roelofs <jameskilton@gmail.com>
2009-04-11 17:22:15 -0400

    Should ignore stl definitions, and anything marked as _OgrePrivate (don't have all of those yet)

--- wrappers/ogre/build_ogre.rb
+++ wrappers/ogre/build_ogre.rb
@@ -74,11 +74,54 @@ Extension.new "ogre" do |e|
     %w(
       RadixSort
     ).each do |klass|
-      node.classes.find(:all, :name => /^#{klass}/).ignore
+      node.classes.find(:name => /^#{klass}/).ignore
     end
+
+    # Some inner classes are marked as _OgrePrivate, but are actually C++ public
+    # to work around VS compiler bugs (I think). Ignore these.
+    %w(
+      MeshSerializerImpl
+      MeshSerializerImpl_v1_1
+      MeshSerializerImpl_v1_2
+      MeshSerializerImpl_v1_3
+    ).each do |klass|
+      node.classes(klass).ignore
+    end
+
+    # Not sure why I can't easy-find nested classes. rbpp bug?
+    node.classes("ProgressiveMesh").classes("PMFaceVertex").ignore
+    node.classes("InstancedGeometry").classes("OptimisedSubMeshGeometry").ignore
+
+
+    # Something is picking up pure STL containers in Ogre, ignore them until
+    # We figure out what's up (causing file creation to crash ... hard)
+    %w(
+      vector
+      pair
+      map
+      iterator
+      list
+    ).each do |klass|
+      if found = node.structs.find(:all, :name => /^#{klass}/)
+        if found.is_a?(Array) && found.empty?
+          # nop
+        else
+          found.ignore
+        end
+      end
+
+      if found = node.classes.find(:all, :name => /^#{klass}/)
+        if found.is_a?(Array) && found.empty?
+          # nop
+        else
+          found.ignore
+        end
+      end
+    end
+    
 
     # TODO Hand build wrappers for the _Iterator classes, we can't auto-gen
     # the wrapping for this stuff and it only blows up
-    node.classes.find(:all, :name => /Iterator/).ignore
+    node.classes.find(:name => /Iterator/).ignore
   end
 end
